0e9e28da34b7f5161e6dd23a5cd2477078ebdd59
Xenorsek <44492935+Xenorsek@users.noreply.github.com>
2018-10-26 10:28:27 +0200

    Create Kod c++.cpp

---
+++ Kod c++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int liczba_do_zadania = 7;
+if(liczba_do_zadania=5){
+	cout<<"Liczba wynosi 5"<<endl;
+}
+else if(liczba_do_zadania==7)
+{
+	cout<<"liczba to "<<endl;
+}
+else
+{
+	cout<<"liczba to cos innego niz 5 i 7 "<<endl;
+}
+return 0;
+}
